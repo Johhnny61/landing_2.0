5a7574b951ee0012f6604f3503c3cb69278e297a
Johhnny61 <johnnyakworlds@gmail.com>
2025-06-25 10:38:46 +0600

    fixed padding of hero

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,19 +5,28 @@ import Hero from '../components/Hero';
 import Steps from '../components/Steps';
 import ContactOrderSection from '../components/ContactOrderSection';
 import Reviews from '../components/Reviews';
+import styled from 'styled-components';
+
+const StyledMain = styled.main`
+  padding-top: 72px; /* Высота заголовка для десктопа */
+
+  @media (max-width: 600px) {
+    padding-top: 56px; /* Высота заголовка для мобильных устройств */
+  }
+`;
 
 export default function Home() {
   return (
     <>
       <Header />
-      <main style={{ paddingTop: 80 }}>
+      <StyledMain>
         <Hero />
         <About />
         <Gallery />
         <Steps />
         <Reviews />
         <ContactOrderSection />
-      </main>
+      </StyledMain>
       <footer style={{textAlign:'center',padding:'32px 0',color:'#888'}}>© {new Date().getFullYear()} SchoolTex</footer>
     </>
   );
